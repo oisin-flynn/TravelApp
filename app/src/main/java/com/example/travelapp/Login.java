01b9ef8de03f849616e722def14c7ff062acf1d6
Oisín <oisinflynn991@gmail.com>
2022-12-19 15:12:45 +0000

    Firebase implemented

--- app/src/main/java/com/example/travelapp/Login.java
+++ app/src/main/java/com/example/travelapp/Login.java
@@ -8,9 +8,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Login extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://travelapp-2abe1-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +42,37 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(Login.this, "Please enter your mobile or password", Toast.LENGTH_SHORT).show();
                 }
                 else {
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
 
+                            // check if phone exists in firebase database
+                            if (snapshot.hasChild(phoneTxt)){
+
+                                // mobile exists in firebase database
+                                // now get password of user from firebase data and match it with user entered password
+                                final String getPassword = snapshot.child(phoneTxt).child("password").getValue(String.class);
+
+                                if (getPassword.equals(passwordTxt)){
+                                    Toast.makeText(Login.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+                                    // open main activity
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(Login.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else {
+                                Toast.makeText(Login.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
             }
         });
