86621d978f4e9bf625941bd613300a27dddb0433
Oisín <oisinflynn991@gmail.com>
2022-12-20 13:58:17 +0000

    MainActivity3 to be finished

--- app/src/main/java/com/example/travelapp/MainActivity3.java
+++ app/src/main/java/com/example/travelapp/MainActivity3.java
@@ -15,14 +15,16 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        btn = (Button)findViewById(R.id.btn1);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
+//        btn = (Button)findViewById(R.id.btn1);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(view.getContext(), MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
 
     }
