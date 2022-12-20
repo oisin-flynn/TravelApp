a0622e22dc5fc9500bd68519aa8cad6fbfb54d91
Oisín <oisinflynn991@gmail.com>
2022-12-20 16:58:47 +0000

    TabLayout to be finished

--- app/src/main/java/com/example/travelapp/HomeFragment.java
+++ app/src/main/java/com/example/travelapp/HomeFragment.java
@@ -17,38 +17,11 @@ import java.util.ArrayList;
 
 
 public class HomeFragment extends Fragment implements MyAdapter.onWonderClickListener {
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
     private ArrayList<WondersInfo> wonderArrayList;
     private String[] wonderHeading;
     private int[] imageResourceID;
     private RecyclerView recyclerView;
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/example/travelapp/MainActivity3.java
+++ app/src/main/java/com/example/travelapp/MainActivity3.java
@@ -1,11 +1,16 @@
 package com.example.travelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity3 extends AppCompatActivity {
     Button btn;
@@ -14,6 +19,17 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new Fragment1(), "Tab 1");
+        vpAdapter.addFragment(new Fragment2(), "Tab 2");
+        vpAdapter.addFragment(new Fragment2(), "Tab 3");
+        viewPager.setAdapter(vpAdapter);
 
 
 
